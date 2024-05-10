b1030d005f3cb6bf441ea88237215e943dd625b0
karlpn <91100704+karlpn@users.noreply.github.com>
2024-05-10 14:28:55 +0200

    auth-register second commit

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,15 +1,18 @@
 export async function register(url, data) {
   const response = await fetch(url, {
     headers: {
-      'Content-Type': 'application-json',
+      'Content-Type': 'application/json',
     },
     method: 'POST',
     body: JSON.stringify(data),
   });
 
-  console.log(response);
+  const json = await response.json();
 
   if (response.ok) {
-    return await response.json();
+    window.location.href = '/sign-in/index.html';
+    return json;
   }
+  document.querySelector('.error-container').textContent =
+    json.errors[0].message;
 }
