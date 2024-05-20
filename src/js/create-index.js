d34fd1c2836c393d5c5c385ef56f3c5298570a83
karlpn <91100704+karlpn@users.noreply.github.com>
2024-05-20 10:49:18 +0200

    bidding second commit

--- src/js/create-index.js
+++ src/js/create-index.js
@@ -2,6 +2,7 @@ import { createListingListener } from './listeners/listings/createListing.js';
 import { checkIfLoggedIn } from './listeners/utilities/checkProfileLogIn.js';
 import { logout } from './api/auth/logout.js';
 import { changeToSignIn } from './listeners/utilities/changeToSignIn.js';
+import { footerUserData } from './listeners/utilities/footerUserData.js';
 
 const check = checkIfLoggedIn('create');
 if (check) {
@@ -9,6 +10,8 @@ if (check) {
 } else {
   changeToSignIn();
 }
+
+footerUserData();
 
 document.querySelector('#logOutBtn').onclick = function () {
   logout();
--- src/js/single-listing-index.js
+++ src/js/single-listing-index.js
@@ -4,6 +4,7 @@ import { logout } from './api/auth/logout.js';
 import { changeToSignIn } from './listeners/utilities/changeToSignIn.js';
 import { displayMessage } from './listeners/utilities/displayMessage.js';
 import { userBidListener } from './listeners/listings/userBid.js';
+import { footerUserData } from './listeners/utilities/footerUserData.js';
 
 const searchParams = new URLSearchParams(window.location.search);
 const id = searchParams.get('id');
@@ -12,6 +13,7 @@ const url = `${API_BASE_URL}auction/listings/${id}/?_seller=true&_bids=true`;
 
 singleListing(url);
 userBidListener(id);
+footerUserData();
 
 document.querySelector('#logOutBtn').onclick = function () {
   logout();
@@ -21,8 +23,9 @@ const profile = JSON.parse(localStorage.getItem('profile'));
 if (!profile) {
   changeToSignIn();
   const message = displayMessage(
-    'You have to be logged in to bid on auction listings!',
+    'You have to be logged in to view bids or bid on auction listings!',
   );
   document.querySelector('form').appendChild(message);
   document.querySelector('#bidAmountInput').disabled = true;
+  document.querySelector('#viewBidBtn').disabled = true;
 }
